5329da9df701b96824848d70ac1d3f1aa287640a
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2026-04-13 17:17:06 +0100

    resonance vs internal pressure not cancelling

--- bunny.py
+++ bunny.py
@@ -10,24 +10,28 @@ from acoustools.Constants import wavelength,k
 import torch
 import os
 
-board = TRANSDUCERS
+board = TOP_BOARD
 p = create_points(1,1,0,0,-0.02)
 
 path = "../BEMMedia"
 
-reflector = load_scatterer(path + '/sphere-lam2.stl')
+reflector = load_scatterer(path + '/bunny-lam6.stl', rotz=90, dz=-0.05)
+# reflector = load_scatterer(path + '/sphere-lam6.stl')
 
-d = wavelength*3
 
-scale_to_diameter(reflector, d)
-centre_scatterer(reflector)
+# d = wavelength*2
 
-H = get_cache_or_compute_H(reflector, board, path=path, use_cache_H=False)
+# scale_to_diameter(reflector, d)
+# centre_scatterer(reflector)
+
+
+
+H = get_cache_or_compute_H(reflector, board, path=path, use_cache_H=False, method='OLS')
 E = compute_E(reflector, p, board, H=H)
 x = iterative_backpropagation(p, board=board, A=E)
 
-internal_points  = get_CHIEF_points(reflector, P = 60, start='centre', method='uniform', scale=0.4, scale_mode='diameter-scale')
-H_CHIEF = get_cache_or_compute_H(reflector, board, path=path, use_cache_H=False, internal_points=internal_points)
+internal_points  = get_CHIEF_points(reflector, P = 40, start='centre', method='uniform', scale=0.08, scale_mode='diameter-scale')
+H_CHIEF = get_cache_or_compute_H(reflector, board, path=path, use_cache_H=False, internal_points=internal_points, method='OLS')
 E_CHIEF = compute_E(reflector, p, board, H=H_CHIEF)
 x_CHIEF = iterative_backpropagation(p, board=board, A=E_CHIEF)
 
@@ -35,11 +39,11 @@ def compute_force_z(activations,points,board, H=None,scatterer=None, path=None):
     force = BEM_compute_force(activations,points,board, H=H, scatterer=scatterer, path=path)[2,:].unsqueeze(0)
     return force
 
-Visualise(*ABC(0.04, origin=p), [x,x_CHIEF], res = (100,100), points=p,
-        colour_functions=[propagate_BEM_phase, propagate_BEM_phase,'-'],
+Visualise(*ABC(0.04, origin=p), [x,x_CHIEF], res = (100,100),
+        colour_functions=[propagate_BEM_pressure, propagate_BEM_pressure,'-'],
         colour_function_args=[{'path':path, 'board':board, 'scatterer':reflector, "H":H},
                                 {'path':path, 'board':board, 'scatterer':reflector, "H":H_CHIEF},
                                 {}],
         link_ax=[0,1],
-        cmaps=['hsv','hsv', 'hsv']
+        # cmaps=['hsv','hsv', 'hsv']
         )
