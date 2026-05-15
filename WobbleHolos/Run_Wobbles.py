124dca4896d862abcfaafcc65acaba75b2925fce
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2026-05-15 16:58:33 +0100

    wobble tunnel

---
+++ WobbleHolos/Run_Wobbles.py
@@ -0,0 +1,13 @@
+from acoustools.Export.Holo import load_holograms
+import os
+
+folder = './WobbleHolos\CHIEF'
+
+holos = []
+
+for i,f in enumerate(os.listdir(folder)):
+    print(i, end='\r')
+    x = load_holograms(folder+'/'+f)[0]
+    holos.append(x)
+
+# print(holos)
