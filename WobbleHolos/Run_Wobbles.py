98dc4174682a09489db66045dc2170e906b3fdd2
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2026-05-29 16:25:59 +0100

    CHEIF vs BEM

--- WobbleHolos/Run_Wobbles.py
+++ WobbleHolos/Run_Wobbles.py
@@ -1,7 +1,9 @@
 from acoustools.Export.Holo import load_holograms
+from acoustools.Levitator import LevitatorController
+from acoustools.Utilities import batch_list
 import os
 
-folder = './WobbleHolos\CHIEF'
+folder = 'data\compare_reflector_wobble\Z-holos\BEM'
 
 holos = []
 
@@ -10,4 +12,18 @@ for i,f in enumerate(os.listdir(folder)):
     x = load_holograms(folder+'/'+f)[0]
     holos.append(x)
 
-print(holos)
+holos.reverse()
+print(len(holos))
+with LevitatorController(ids=(87)) as lev:
+
+    lev.set_frame_rate(275)
+
+    lev.levitate(holos[500])
+
+    input()
+    exit()
+    for batch in batch_list(holos, batch=1):
+        lev.levitate(batch)
+        # input()
+    print('Done')
+    input()
