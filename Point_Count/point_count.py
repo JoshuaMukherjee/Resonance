5acd6fec58b66c1faaca1b8645cdf47cbde48815
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2026-07-27 11:37:10 +0100

    error bar

--- Point_Count/point_count.py
+++ Point_Count/point_count.py
@@ -14,31 +14,40 @@ x = kd_solver(create_points(1,1,0,0,0), board=board)
 ds_frac = [1, 1.5, 2, 2.5 ,3 ]
 ds = [d * wavelength for d in ds_frac]
 
-points = [1, 5, 10, 20, 30, 40, 50, 100, 200, 500, 1000]
+# points = [1, 5, 10, 20, 30, 40, 50, 100, 200, 500, 1000]
+points = [1, 5]
 
+
+repeats = 2
 
 path = '../BEMMedia/'
 scatterer_name = 'sphere-lam4.stl'
 
+
 for i,d in enumerate(ds):
-    scatterer = load_scatterer(path+scatterer_name, root_path=path)
-    centre_scatterer(scatterer)
-    scale_to_diameter(scatterer, d)
-
-    A = compute_A(scatterer)
-    p = get_tetra_centroids(scatterer)
-
     pressures = []
+    pressure_to_plot = []
+    for rep in range(repeats):
+        pressures.append([])
 
-    for P in points:
-        print(d, P, end='\r')
+        scatterer = load_scatterer(path+scatterer_name, root_path=path)
+        centre_scatterer(scatterer)
+        scale_to_diameter(scatterer, d)
 
-        internal_points = get_CHIEF_points(scatterer, P=P, method = 'tetra-random')
-        A_CHIEF = augment_A_CHIEF(A, internal_points=internal_points, scatterer=scatterer)
-        H = compute_H(scatterer, board=board, A=A_CHIEF, internal_points=internal_points)
-        press = propagate_BEM_pressure(x, points=p, board=board, H=H, scatterer=scatterer, path=path).mean()
-        pressures.append(press.item())
-    
+        A = compute_A(scatterer)
+        p = get_tetra_centroids(scatterer)
+
+       
+
+        for P in points:
+            print(d, P, end='\r')
+
+            internal_points = get_CHIEF_points(scatterer, P=P, method = 'tetra-random')
+            A_CHIEF = augment_A_CHIEF(A, internal_points=internal_points, scatterer=scatterer)
+            H = compute_H(scatterer, board=board, A=A_CHIEF, internal_points=internal_points)
+            press = propagate_BEM_pressure(x, points=p, board=board, H=H, scatterer=scatterer, path=path).mean()
+            pressures[i].append(press.item())
+        pressure_to_plot.append(sum(pressures[i]) / repeats)
     plt.plot(points, pressures, marker='x', label=f'${ds_frac[i]}\lambda$')
 
 
